ff7121665e0916cc414892e945b8a6677ad9994c
임준형 <haechilim@gmail.com>
2021-08-14 15:59:38 +0900

    리팩토링: list.js class화

--- js/list.js
+++ js/list.js
@@ -1,157 +1,163 @@
-const POST_PER_PAGE = 10;
-const sort = getParameters().sort;
+class List {
+    static POST_PER_PAGE = 10;
+
+    constructor() {
+        this.sort = this.getParameters().sort;
+    }
+
+    init() {
+        this.selectedNewSortCategory();
+        this.selectedTopSortCategory();
+        
+        this.request("/api/count", (result) => {
+            console.log(result, this);
+            this.drawPageButton(result[0].count);
+            this.updateList();
+        });
+    }
+    
+    updateList() {
+        this.request("/api/list?limit=" + List.POST_PER_PAGE + "&offset=" + (this.getParameters().page - 1) * List.POST_PER_PAGE + "&sort=" + this.getParameters().sort, (posts) => {
+            this.drawPosts(posts);
+            this.bindEvents();
+        });
+    }
+    
+    bindEvents() {
+        document.getElementById("writeButton").addEventListener("click", () => {
+            location.href = "/write";
+        });
+    
+        document.getElementById("new").addEventListener("click", () => {
+            location.href = "/?sort=new&page=1";
+        });
+    
+        document.getElementById("top").addEventListener("click", () => {
+            location.href = "/?sort=top&page=1";
+        });
+    
+        document.getElementById("pageBefore").addEventListener("click", () => {
+            location.href = "/?sort=" + this.sort + "&page=" + (parseInt(this.getParameters().page) - 1);
+        });
+    
+        document.getElementById("pageNext").addEventListener("click", () => {
+            location.href = "/?sort=" + this.sort + "&page=" + (parseInt(this.getParameters().page) + 1);
+        });
+    }
+    
+    request(url, callback) {
+        let xmlHttpRequest = new XMLHttpRequest();
+    
+        xmlHttpRequest.addEventListener("load", () => {
+            let json = JSON.parse(xmlHttpRequest.responseText);
+            
+            if(callback) callback(json);
+        });
+    
+        xmlHttpRequest.open("GET", url, true);
+        xmlHttpRequest.send();
+    }
+    
+    drawPageButton(count) {
+        const pageCount = this.getPageCount(count);
+        const currentPage = this.getParameters().page;
+    
+        let html = "";
+    
+        for(let i = 1; i <= pageCount; i++) {
+            let isCurrentPage = (i == currentPage);
+    
+            html += "<button class=\"pageButton " + (isCurrentPage ? "currentPage" : "") + "\" >" + (isCurrentPage ? i : "<a href=\"?sort=" + this.sort + "&page=" + i + "\">" + i + "</a>") + "</button>";
+        }
+    
+        document.getElementById("pageButtonContainer").innerHTML = html;
+    
+        showBeforeButton(currentPage != 1);
+        showNextButton(currentPage != pageCount);
+        console.log(pageCount);
+    
+        function showBeforeButton(visibility) {
+            document.getElementById("pageBefore").style.visibility = visibility ? "visibility" : "hidden";
+        }
+    
+        function showNextButton(visibility) {
+            document.getElementById("pageNext").style.visibility = visibility ? "visibility" : "hidden";
+        }
+    }
+    
+    drawPosts(posts) {
+        console.log(posts);
+    
+        let html = "";
+    
+        for(let i = 0; i < posts.length; i++) {
+            html += this.getPostHtml(posts[i]);
+        }
+    
+        document.querySelector(".list").innerHTML = html;
+    }   
+    
+    getPostHtml(post) {
+        return "<div class=\"post\">\n" +
+        "            <div class=\"recommendationContainer\">\n" +
+        "                <img class=\"image\" src=\"image/recommendation.png\"/>\n" +
+        "                <div class=\"recommendation\">" + (post.like - post.dislike) + "</div>\n" +
+        "            </div>\n" +
+        "\n" +
+        "            <div class=\"postInformation\">\n" +
+        "                <div class=\"titleContainer\">\n" +
+        "                    <div class=\"title\"><a href=\"contents?id=" + post.id + "\">" + post.title + "</a></div>\n" +
+        "                    <div class=\"comment\">[" + post.ccount + "]</div>\n" +
+        "                </div>\n" +
+        "\n" +
+        "                <div class=\"writerContainer\">\n" +
+        "                    <div class=\"type\">" + post.category + "</div>\n" +
+        "                    <div class=\"time\">" + post.dateTime + "</div>\n" +
+        "                    <div class=\"writer\">\n" +
+        "                        <img class=\"image\" src=\"image/level.png\">\n" +
+        "                        <div class=\"name\">" + post.mname + "</div>\n" +
+        "                    </div>\n" +
+        "                </div>\n" +
+        "            </div>\n" +
+        "        </div>"
+    }
+    
+    getPageCount(postCount) {
+        return Math.ceil(postCount / List.POST_PER_PAGE);
+    }
+    
+    getParameters() {
+        let result = {};
+        let part = parameterPart();
+        let parameters = part.split("&");
+        
+        for(let i = 0; i < parameters.length; i++) {
+            let tokens = parameters[i].split("=");
+            
+            if(tokens.length < 2) continue;
+            
+            result[tokens[0]] = tokens[1];
+        }
+        
+        return result;
+        
+        function parameterPart() {
+            let tokens = location.search.split("?");
+            
+            return tokens.length > 1 ? tokens[1] : "";
+        }
+    }
+    
+    selectedNewSortCategory() {
+        document.getElementById("new").className = this.sort == "new" ? "newOn" : "new";
+    }
+    
+    selectedTopSortCategory() {
+        document.getElementById("top").className = this.sort == "top" ? "topOn" : "top";
+    }
+}
 
 document.addEventListener("DOMContentLoaded", function() {
-    init();
+    new List().init();
 });
 
-function init() {
-    selectedNewSortCategory(sort);
-    selectedTopSortCategory(sort);
-
-    request("/api/count", (result) => {
-        drawPageButton(result[0].count);
-        updateList();
-    });
-}
-
-function updateList() {
-    request("/api/list?limit=" + POST_PER_PAGE + "&offset=" + (getParameters().page - 1) * POST_PER_PAGE + "&sort=" + getParameters().sort, (posts) => {
-        drawPosts(posts);
-        bindEvents();
-    });
-}
-
-function bindEvents() {
-    document.getElementById("writeButton").addEventListener("click", () => {
-        location.href = "/write";
-    });
-
-    document.getElementById("new").addEventListener("click", () => {
-        location.href = "/?sort=new&page=1";
-    });
-
-    document.getElementById("top").addEventListener("click", () => {
-        location.href = "/?sort=top&page=1";
-    });
-
-    document.getElementById("pageBefore").addEventListener("click", () => {
-        location.href = "/?sort=" + sort + "&page=" + (parseInt(getParameters().page) - 1);
-    });
-
-    document.getElementById("pageNext").addEventListener("click", () => {
-        location.href = "/?sort=" + sort + "&page=" + (parseInt(getParameters().page) + 1);
-    });
-}
-
-function request(url, callback) {
-    let xmlHttpRequest = new XMLHttpRequest();
-
-    xmlHttpRequest.addEventListener("load", () => {
-        let json = JSON.parse(xmlHttpRequest.responseText);
-        
-		if(callback) callback(json);
-	});
-
-	xmlHttpRequest.open("GET", url, true);
-	xmlHttpRequest.send();
-}
-
-function drawPageButton(count) {
-    const pageCount = getPageCount(count);
-    const currentPage = getParameters().page;
-    console.log(sort);
-
-    let html = "";
-
-    for(let i = 1; i <= pageCount; i++) {
-        let isCurrentPage = (i == currentPage);
-
-        html += "<button class=\"pageButton " + (isCurrentPage ? "currentPage" : "") + "\" >" + (isCurrentPage ? i : "<a href=\"?sort=" + sort + "&page=" + i + "\">" + i + "</a>") + "</button>";
-    }
-
-    document.getElementById("pageButtonContainer").innerHTML = html;
-
-    showBeforeButton(currentPage != 1);
-    showNextButton(currentPage != pageCount);
-    console.log(pageCount);
-
-    function showBeforeButton(visibility) {
-        document.getElementById("pageBefore").style.visibility = visibility ? "visibility" : "hidden";
-    }
-
-    function showNextButton(visibility) {
-        document.getElementById("pageNext").style.visibility = visibility ? "visibility" : "hidden";
-    }
-}
-
-function drawPosts(posts) {
-    console.log(posts);
-
-    let html = "";
-
-    for(let i = 0; i < posts.length; i++) {
-        html += getPostHtml(posts[i]);
-    }
-
-    document.querySelector(".list").innerHTML = html;
-}   
-
-function getPostHtml(post) {
-    return "<div class=\"post\">\n" +
-    "            <div class=\"recommendationContainer\">\n" +
-    "                <img class=\"image\" src=\"image/recommendation.png\"/>\n" +
-    "                <div class=\"recommendation\">" + (post.like - post.dislike) + "</div>\n" +
-    "            </div>\n" +
-    "\n" +
-    "            <div class=\"postInformation\">\n" +
-    "                <div class=\"titleContainer\">\n" +
-    "                    <div class=\"title\"><a href=\"contents?id=" + post.id + "\">" + post.title + "</a></div>\n" +
-    "                    <div class=\"comment\">[" + post.ccount + "]</div>\n" +
-    "                </div>\n" +
-    "\n" +
-    "                <div class=\"writerContainer\">\n" +
-    "                    <div class=\"type\">" + post.category + "</div>\n" +
-    "                    <div class=\"time\">" + post.dateTime + "</div>\n" +
-    "                    <div class=\"writer\">\n" +
-    "                        <img class=\"image\" src=\"image/level.png\">\n" +
-    "                        <div class=\"name\">" + post.mname + "</div>\n" +
-    "                    </div>\n" +
-    "                </div>\n" +
-    "            </div>\n" +
-    "        </div>"
-}
-
-function getPageCount(postCount) {
-    return Math.ceil(postCount / POST_PER_PAGE);
-}
-
-function getParameters() {
-    let result = {};
-    let part = parameterPart();
-    let parameters = part.split("&");
-    
-    for(let i = 0; i < parameters.length; i++) {
-        let tokens = parameters[i].split("=");
-        
-        if(tokens.length < 2) continue;
-        
-        result[tokens[0]] = tokens[1];
-    }
-    
-    return result;
-    
-    function parameterPart() {
-        let tokens = location.search.split("?");
-        
-        return tokens.length > 1 ? tokens[1] : "";
-    }
-}
-
-function selectedNewSortCategory(sort) {
-    document.getElementById("new").className = sort == "new" ? "newOn" : "new";
-}
-
-function selectedTopSortCategory(sort) {
-    document.getElementById("top").className = sort == "top" ? "topOn" : "top";
-}
